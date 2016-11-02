641e73698c63fc9825a0eda8426acec470c75ba0
jredd <jredd46@gmail.com>
2016-11-02 15:09:56 -0500

    move the file structure around

--- stream/admin.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.FourVideoStream)
-admin.site.register(models.SixVideoStream)
