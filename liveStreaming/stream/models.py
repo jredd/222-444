0552ae1c40fe040de811fa8e3cb60071109615b9
jredd <jredd46@gmail.com>
2016-11-09 15:55:06 -0600

    adding the two stream models and views

--- liveStreaming/stream/models.py
+++ liveStreaming/stream/models.py
@@ -3,6 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class TwoVideoStream(models.Model):
+
+    title = models.CharField(max_length=60)
+    video1 = models.CharField(max_length=40)
+    video2 = models.CharField(max_length=40)
+    chat = models.CharField(max_length=40, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
 class FourVideoStream(models.Model):
 
     title = models.CharField(max_length=60)
--- liveStreaming/stream/views.py
+++ liveStreaming/stream/views.py
@@ -7,20 +7,28 @@ from . import models
 
 class IndexView(generic.ListView):
     template_name = 'index.html'
-    context_object_name = 'four'
-    queryset = models.FourVideoStream.objects.all()
+    context_object_name = 'two'
+    queryset = models.TwoVideoStream.objects.all().order_by('title')
 
     def get_context_data(self, **kwargs):
         """Return the six video list as well"""
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['six'] = models.SixVideoStream.objects.order_by('title')
+        context['four'] = models.FourVideoStream.objects.all().order_by('title')
+        context['six'] = models.SixVideoStream.objects.all().order_by('title')
         return context
+
+
+class TwoStreamView(generic.DetailView):
+    template_name = 'two_streams.html'
+    context_object_name = 'stream'
+    queryset = models.TwoVideoStream.objects.all()
 
 
 class FourStreamView(generic.DetailView):
     template_name = 'four_streams.html'
     context_object_name = 'stream'
     queryset = models.FourVideoStream.objects.all()
+
 
 class SixStreamView(generic.DetailView):
     template_name = 'six_streams.html'
