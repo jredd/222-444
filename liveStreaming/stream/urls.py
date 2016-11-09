9c25b78a71895a3dc104397936650a96382b9a83
jredd <jredd46@gmail.com>
2016-11-09 15:59:12 -0600

    adding the two stream urls

--- liveStreaming/stream/urls.py
+++ liveStreaming/stream/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url('two/(?P<pk>[a-z-A-Z-0-9]+)/$', views.TwoStreamView.as_view()),
     url('four/(?P<pk>[a-z-A-Z-0-9]+)/$', views.FourStreamView.as_view()),
     url('six/(?P<pk>[a-z-A-Z-0-9]+)/$', views.SixStreamView.as_view()),
 ]
